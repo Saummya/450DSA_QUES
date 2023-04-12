b36b0ddddcd008d75cf1a0a6ad584f495783fb2d
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2023-04-13 01:08:33 +0530

    Create Sort an array of 0s, 1s and 2s without sorting.py

---
+++ ARRAYS/Sort an array of 0s, 1s and 2s without sorting.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+class Solution:
+    def sort012(self,arr,n):
+        # code here
+        
+        c0=arr.count(0) #2
+        c1=arr.count(1) #1
+        c2=arr.count(2) #2
+        
+        for i in range(n):   #0,0,1,2,2
+            if c0!=0:
+                arr[i]=0
+                c0-=1
+            elif c1!=0:
+                arr[i]=1
+                c1-=1
+            else:
+                arr[i]=2
+                
+        return arr
+        
+        
+        
+      
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.sort012(arr,n)
+        for i in arr:
+            print(i, end=' ')
+        print()
+
+# } Driver Code Ends
