562990cf04f1778ef50189058daddc09a9bcec29
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2023-04-15 20:05:25 +0530

    Create Union and intersection of two arrays.py

---
+++ ARRAYS/Union and intersection of two arrays.py
@@ -0,0 +1,75 @@
+'''
+Given two arrays a[] and b[] of size n and m respectively. The task is to find the number of elements in the union between these two arrays.
+
+Union of the two arrays can be defined as the set containing distinct elements from both the arrays. If there are repetitions, then only one occurrence of element should be printed in the union.
+
+Note : Elements are not necessarily distinct.
+
+Example 1:
+
+Input:
+5 3
+1 2 3 4 5
+1 2 3
+Output: 
+5
+Explanation: 
+1, 2, 3, 4 and 5 are the
+elements which comes in the union set
+of both arrays. So count is 5.
+Example 2:
+
+Input:
+6 2 
+85 25 1 32 54 6
+85 2 
+Output: 
+7
+Explanation: 
+85, 25, 1, 32, 54, 6, and
+2 are the elements which comes in the
+union set of both arrays. So count is 7.
+'''
+
+#User function Template for python3
+
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def doUnion(self,a,n,b,m):
+        
+        #code here
+        '''
+        s=set()
+        
+        for i in range(n):
+            s.add(a[i])
+            
+        for i in range(m):
+            s.add(b[i])
+            
+        return len(s)
+        '''
+        
+        a=set(a)
+        b=set(b)
+        
+        return len(a.union(b))
+       
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        
+        print(ob.doUnion(a,n,b,m))
+# } Driver Code Ends
